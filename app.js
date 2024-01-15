3caede380f6fe194767dae2b4c0c54a33acc9914
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2024-01-15 16:13:22 +0100

    session route

--- app.js
+++ app.js
@@ -4,30 +4,18 @@ const debug= require('debug')('app');
 const morgan=require('morgan');
 const path=require('path');
 
+
 const PORT= process.env.PORT || 3000;
 const app=express();
-const sessionsRouter=express.Router();
+const sessionsRouter=require('./src/routers/sessionsRouters');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public/')));
 
-sessionsRouter.route('/').get((req,res)=>{
-    res.render("sessions",{sessions:[
-        {title:"Session 1", description:'this is session 1'},
-        {title:"Session 2", description:'this is session 2'},
-        {title:"Session 3", description:'this is session 3'},
-        {title:"Session 4", description:'this is session 4'}
-    ]});
-})
-
-sessionsRouter.route('/1').get((req,res)=>{
-    res.send("hello single sessions");
-})
-
-app.use('/sessions',sessionsRouter);
-
 app.set('views','./src/views');
 app.set('view engine','ejs');
+
+app.use('/sessions',sessionsRouter);
 
 app.get('/',(req,res)=>{
     res.render('index',{title:'Welcome to Globomantics', data:['a','b','c']});
